1c5754ba6d8c1c2d7af5bc5d0d4a7ef11d105582
Sunhaolin <sunhaolin@hotoa.com>
2021-03-29 17:31:32 +0800

    统一package servicename规则

--- services/steedos-app.service.js
+++ services/steedos-app.service.js
@@ -3,23 +3,21 @@ const path = require('path');
 const project = require('../steedos-app/package.json');
 const packageName = project.name;
 const packageLoader = require('@steedos/service-package-loader');
-const serviceName = `$packages-${packageName}`;
 /**
  * @typedef {import('moleculer').Context} Context Moleculer's Context
  * 软件包服务启动后也需要抛出事件。
  */
 module.exports = {
-	name: serviceName,
+	name: packageName,
 	namespace: "steedos",
 	mixins: [packageLoader],
-  dependencies: ['metadata-server'],
+	dependencies: ['metadata-server'],
 	/**
 	 * Settings
 	 */
 	settings: {
 		packageInfo: {
 			path: path.join(__dirname, '../steedos-app'),
-			name: serviceName
 		}
 	},
 
